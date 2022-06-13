6f0ebc33aef5d07b567f403e83ae37aa5ffa027f
ashwin <ashwinam31@gmail.com>
2022-06-13 14:00:51 +0530

    assignment js file update

--- fundamentals-part-1/assignment.js
+++ fundamentals-part-1/assignment.js
@@ -3,10 +3,36 @@
 // assign their values according to your own country (population in millions)
 // 2. Log their values to the console
 
-let country = 'India';
-let continent = 'Asia';
+const country = 'India';
+const continent = 'Asia';
 let population = 1350
 
 console.log(country)
 console.log(continent)
 console.log(population + " million")
+
+/*
+LECTURE: Data Types
+1. Declare a variable called 'isIsland' and set its value according to your
+country. The variable should hold a Boolean value. Also declare a variable
+'language', but don't assign it any value yet
+2. Log the types of 'isIsland', 'population', 'country' and 'language'
+to the console */
+
+// when we declare variable need to use let keyword
+const isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+
+/* LECTURE: let, const and var
+1. Set the value of 'language' to the language spoken where you live (some
+countries have multiple languages, but just choose one)
+2. Think about which variables should be const variables (which values will never
+change, and which might change?). Then, change these variables to const.
+3. Try to change one of the changed variables now, and observe what happens */
+
+language = "marathi";
--- fundamentals-part-1/script.js
+++ fundamentals-part-1/script.js
@@ -1,4 +1,17 @@
-let js = "amazing";
-if (js === "amazing") alert("JavaScript is Fun!");
-// for printing in console
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// if (js === "amazing") alert("JavaScript is Fun!");
+// // for printing in console
+// console.log(40 + 8 + 23 - 10);
+
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+// null type of shows the object type
+let coding;
+let code = null;
+console.log(typeof javaScriptIsFun);
+console.log(typeof "javaScriptIsFun");
+console.log(typeof 500);
+console.log(typeof code);
+console.log(typeof coding);
+console.log(typeof 23.50);
