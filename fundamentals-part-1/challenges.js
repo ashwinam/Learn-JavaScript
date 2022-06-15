260caead39336e053bea9fddc1a640097888dd60
ashwin <ashwinam31@gmail.com>
2022-06-15 13:16:27 +0530

    string & if else done

---
+++ fundamentals-part-1/challenges.js
@@ -0,0 +1,57 @@
+/*
+Coding Challenge #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+GOOD LUCK 😀
+*/
+
+// Test 1
+
+// let marksHeight = 1.69;
+// let marksMass = 78;
+
+// let marksBMI = marksMass / marksHeight ** 2;
+
+// console.log(marksBMI);
+
+// let johnHeight = 1.95;
+// let johnMass = 92;
+// let johnBMI = johnMass / johnHeight ** 2;
+
+// console.log(johnBMI)
+
+// let markHeigherBMI = marksBMI > johnBMI;
+// console.log(markHeigherBMI)
+
+
+// // Test 2
+// marksMass = 95;
+// marksHeight = 1.88;
+
+// marksBMI = marksMass / marksHeight ** 2;
+
+// console.log(marksBMI);
+
+// johnMass = 85;
+// johnHeight = 1.76;
+// johnBMI = johnMass / johnHeight ** 2;
+
+// console.log(johnBMI)
+
+
+
+// markHeigherBMI = marksBMI > johnBMI;
+// console.log(markHeigherBMI)
--- fundamentals-part-1/script.js
+++ fundamentals-part-1/script.js
@@ -4,14 +4,70 @@
 // console.log(40 + 8 + 23 - 10);
 
 
-let javaScriptIsFun = true;
-console.log(javaScriptIsFun);
-// null type of shows the object type
-let coding;
-let code = null;
-console.log(typeof javaScriptIsFun);
-console.log(typeof "javaScriptIsFun");
-console.log(typeof 500);
-console.log(typeof code);
-console.log(typeof coding);
-console.log(typeof 23.50);
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+// // null type of shows the object type
+// let coding;
+// let code = null;
+// console.log(typeof javaScriptIsFun);
+// console.log(typeof "javaScriptIsFun");
+// console.log(typeof 500);
+// console.log(typeof code);
+// console.log(typeof coding);
+// console.log(typeof 23.50);
+
+
+
+// string literals
+
+const firstName = 'ashwin';
+const job = 'developer';
+const birthYear = 1995;
+const year = 2023;
+
+// concatenate string
+let intro = "I'm " + firstName + " currently I'm working as a " + job + " and I'm a " + (year - birthYear) + " Years old!";
+console.log(intro);
+
+
+// same using string literals
+intro = `I'm ${firstName}, currently working as a ${job} and I'm a ${year - birthYear} years old!`;
+console.log(intro);
+
+// normal string using backticks
+console.log(`Hello world!`);
+
+// multiple line
+console.log("Hello, \nNew line \nimplemented");
+console.log(`hello 
+new line
+implemented`);
+
+// Taking Decisions using If, Else statement
+
+// check the veh driver is above 18 or not
+
+const age = 15;
+
+const isAgeEnough = age >= 18;
+
+// if (check condition){
+//     // Do something;
+// }
+
+// what if user not then do else thing.
+
+if (isAgeEnough) {
+    console.log("user is able to start Driving. 🚗");
+} else {
+    console.log("user not at the age");
+}
+
+
+if (age >= 18) {
+    console.log("user is able to start Driving. 🚗");
+} else {
+    console.log(`user not allowed to drive the car, you have to wait ${18 - age} year.`);
+}
+
+// This is called the control structure.
